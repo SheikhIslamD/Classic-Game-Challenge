571a27468f77beceec559f4233552cb87f408bde
Sheikh Islam <143849374+SheikhIslamD@users.noreply.github.com>
2024-02-01 16:08:18 -0500

    Lives and game over
    -added orange sprite, button sprites, pixel fonts
    -modified ball script to count lives and trigger game over
    -added ui canvas with life images and simple game over screen for testing

--- BreakOut_Project/Assets/BallScript.cs
+++ BreakOut_Project/Assets/BallScript.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BallScript : MonoBehaviour
 {
     public float minY = -5.5f;
     public float maxVelocity = 15f;
 
+    public int lives = 3;
+
+    public Image[] livesImage = new Image[4];
+    public GameObject endScreen;
+    public TextMeshProUGUI endText;
+
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
        rb = GetComponent<Rigidbody2D>(); 
+       Time.timeScale = 1;
     }
 
     // Update is called once per frame
@@ -21,6 +30,7 @@ public class BallScript : MonoBehaviour
         {
             transform.position = Vector3.zero;
             rb.velocity = Vector3.zero;
+            LifeUpdate();
         }
 
         if(rb.velocity.magnitude > maxVelocity)
@@ -34,6 +44,29 @@ public class BallScript : MonoBehaviour
         if (collision.gameObject.CompareTag("Brick"))
         {
             Destroy(collision.gameObject);
+        }
+    }
+
+    void LifeUpdate()
+    {
+        lives--;
+        int i = lives;
+        foreach(Image image in livesImage)
+        {
+            if (i >= 0)
+            {
+                image.fillAmount = 1;
+                i--;
+            }
+            else image.fillAmount = 0;
+        }
+        if (lives == 0)
+        {
+            //game over
+            endScreen.SetActive(true);
+            string text = "You Lose!";
+            endText.text = text.ToString();
+            Time.timeScale = 0;
         }
     }
 }
